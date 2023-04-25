278ebdce63587a02bd4ad1b0cf1447f3545c2ee8
Ryder McMinn <mcminnra@gmail.com>
2023-04-24 20:37:16 -0700

    add conda to fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -13,3 +13,11 @@ switch (uname -s)
 end
 
 starship init fish | source
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /Users/mcminnra/miniconda3/bin/conda
+    eval /Users/mcminnra/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+end
+# <<< conda initialize <<<
+
