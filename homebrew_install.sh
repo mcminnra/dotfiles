c2daa8d1578f6fc0a36b66eaf62cf8aab95c29d3
Ryder McMinn <rmcminn@us603e5f4063f1.am.sony.com>
2024-06-05 16:11:09 -0700

    add homebrew install script

---
+++ homebrew_install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Install Formulae
+brew install fish gh lsd jandedobbeleer/oh-my-posh/oh-my-posh bat btop git wget
+
+# Install Casks
+brew install --cask alacritty emacs google-chrome font-sauce-code-pro-nerd-font amethyst visual-studio-code dropbox
+
+
