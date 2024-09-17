6d1535f567f035356793646f4cddd5ab5e53e271
Ryder McMinn <mcminnra@gmail.com>
2024-09-16 23:01:00 -0700

    brew add fastfetch

--- homebrew_install.sh
+++ homebrew_install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Install Formulae
-brew install fish gh lsd jandedobbeleer/oh-my-posh/oh-my-posh bat btop git wget
+brew install fastfetch fish gh lsd jandedobbeleer/oh-my-posh/oh-my-posh bat btop git wget
 
 # Install Casks
 brew install --cask tailscale alacritty emacs google-chrome font-sauce-code-pro-nerd-font amethyst visual-studio-code dropbox spotify discord sqlitestudio syncthing
