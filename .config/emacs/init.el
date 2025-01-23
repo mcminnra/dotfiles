311300ca313b594ff0c95656b98772aaa8a17b5a
Ryder McMinn <rdr@rdrmc.com>
2025-01-23 18:42:31 +0900

    update org agenda

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -359,11 +359,11 @@
   :config
   (setq org-super-agenda-groups
 	'(
-	  (:name "Pursuits" :file-path "/projects/")
 	  (:name "Work Tasks"
 		 :and (:file-path "work_sie.org" :scheduled today :not (:habit t)))
 	  (:name "Personal Tasks"
-		 :and (:file-path "tasks.org" :scheduled today :not (:habit t)))
+		 :and (:file-path "tasks.org" :scheduled today :not (:habit t))
+     :and (:file-path "/notes/" :scheduled today :not (:habit t)))
 	  (:name "Habits"
 		 :habit t)
 	  (:name "Next"
