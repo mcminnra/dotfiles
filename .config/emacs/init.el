139f3a57d68fe60ce522aed3400131e593563be6
Ryder McMinn <rdr@rdrmc.com>
2025-02-27 16:48:57 -0800

    add markdown mode

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -217,6 +217,22 @@
   :after (treemacs evil)
   :ensure t)
 
+
+;; ===============================================
+;; Programming Modes Config
+;; ===============================================
+;; Markdown
+(use-package markdown-mode
+  :ensure t
+  :mode (("README\\.md\\'" . gfm-mode)
+         ("\\.md\\'" . markdown-mode)
+         ("\\.markdown\\'" . markdown-mode))
+  :init (setq markdown-command "multimarkdown"))
+
+(use-package poly-markdown
+  :ensure t)
+
+
 ;; ===============================================
 ;; Org Config
 ;; ===============================================
