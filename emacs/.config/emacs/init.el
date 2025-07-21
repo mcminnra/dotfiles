1b21fda6d6fa0ebf1bf33e24308ca9632933e761
Ryder McMinn <rdr@rdrmc.com>
2025-07-20 20:01:59 -0700

    emacs: improve search

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -90,9 +90,13 @@
 ;; Helm
 (use-package helm
   :config
+  ; Force helm to search inside of a given window
+  (setq helm-split-window-inside-p t) 
+  ; Keybinds
   (global-set-key (kbd "M-x") 'helm-M-x)
   (global-set-key (kbd "C-x r b") #'helm-filtered-bookmarks)
   (global-set-key (kbd "C-x C-f") #'helm-find-files)
+  (global-set-key (kbd "C-s") #'helm-occur)
   (helm-mode 1))
 
 ;; which-key
