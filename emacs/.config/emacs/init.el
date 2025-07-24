fe01fa346870a628ecd88094cc4a02bd9669904c
Ryder McMinn <rdr@rdrmc.com>
2025-07-23 23:16:55 -0700

    emacs: tree sitter todo

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -243,18 +243,9 @@
   (evil-org-agenda-set-keys))  
 
 ;; ===============================================
-;; Programming Modes Config
+;; Tree-sitter
 ;; ===============================================
-;; Markdown
-(use-package markdown-mode
-  :ensure t
-  :mode (("README\\.md\\'" . gfm-mode)
-         ("\\.md\\'" . markdown-mode)
-         ("\\.markdown\\'" . markdown-mode))
-  :init (setq markdown-command "multimarkdown"))
-
-(use-package poly-markdown
-  :ensure t)
+;; TODO
 
 ;; ===============================================
 ;; Org Config
