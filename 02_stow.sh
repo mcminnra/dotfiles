6b6f5a426978821f1ea0fab8854ff04686bc524f
Ryder McMinn <rdr@rdrmc.com>
2025-07-30 10:40:24 -0700

    git: add new config and better os mapping

--- 02_stow.sh
+++ 02_stow.sh
@@ -4,8 +4,20 @@ stow alacritty -t ~/ --verbose=2
 stow emacs -t ~/ --verbose=2
 stow fish -t ~/ --verbose=2
 
-# Condionally ask for some folders
+# === Condionally ask for some folders
+# git
 read -p "Do you want to stow git? (y/n): " answer
 if [[ "$answer" =~ ^[Yy]$ ]]; then
     stow git -t ~/ --verbose=2
+    case "$OSTYPE" in
+        msys|cygwin|win32)
+            stow git-windows -t ~/ --verbose=2
+            ;;
+        darwin*)
+            stow git-macos -t ~/ --verbose=2
+            ;;
+        linux*)
+            stow git-linux -t ~/ --verbose=2
+            ;;
+    esac
 fi
