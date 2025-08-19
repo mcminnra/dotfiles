c54b5e333d5740d7e153db9173f7ae9be622b7b5
Ryder McMinn <rdr@rdrmc.com>
2025-08-18 22:15:22 -0700

    emacs: more evil pkgs

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -230,6 +230,20 @@
     (kbd "]]") 'org-next-visible-heading
     (kbd "[[") 'org-previous-visible-heading))
 
+(use-package evil-collection
+  :after evil
+  :ensure t
+  :config
+  (evil-collection-init))
+
+(use-package evil-org
+  :ensure t
+  :after org
+  :hook (org-mode . (lambda () evil-org-mode))
+  :config
+  (require 'evil-org-agenda)
+  (evil-org-agenda-set-keys))
+
 ;; Treemacs
 (use-package treemacs
   :ensure t
