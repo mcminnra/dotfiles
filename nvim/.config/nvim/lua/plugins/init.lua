6fd12ee805d4582fc39b70e3f77094ff67c213d2
Ryder McMinn <rdr@rdrmc.com>
2025-08-26 01:48:43 -0700

    nvim: add harpoon

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -64,7 +64,7 @@ return {
       },
       indent = { enabled = true },
       input = { enabled = true },
-      picker = { enabled = true },
+      picker = { enabled = true }, -- TODO: Maybe just not use this and replace dashboard cmds with telescope
       notifier = { enabled = true },
       quickfile = { enabled = true },
       scope = { enabled = true },
@@ -138,6 +138,36 @@ return {
       vim.keymap.set("n", "<leader>tc", ":NvimTreeClose<CR>")
       vim.keymap.set("n", "<leader>tl", ":NvimTreeFindFile<CR>")
     end,
+  },
+  {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    keys = {
+      {
+        '<leader>ho',
+        function()
+          local harpoon = require('harpoon')
+          harpoon.ui:toggle_quick_menu(harpoon:list())
+        end,
+        desc = 'List locations'
+      },
+      { '<leader>ha',      function() require('harpoon'):list():add() end,     desc = 'Add Location' },
+      { '<leader>hr',     function() require('harpoon'):list():remove() end,  desc = 'Remove Location' },
+      { '<C-n>',          function() require('harpoon'):list():next() end,    desc = 'Next Location' },
+      { '<C-p>',          function() require('harpoon'):list():prev() end,    desc = 'Previous Location' },
+      { '<leader>1', function() require('harpoon'):list():select(1) end, desc = 'Harpoon to File 1' },
+      { '<leader>2', function() require('harpoon'):list():select(2) end, desc = 'Harpoon to File 2' },
+      { '<leader>3', function() require('harpoon'):list():select(3) end, desc = 'Harpoon to File 3' },
+      { '<leader>4', function() require('harpoon'):list():select(4) end, desc = 'Harpoon to File 4' },
+      { '<leader>5', function() require('harpoon'):list():select(5) end, desc = 'Harpoon to File 5' },
+    },
+    config = function()
+      local harpoon = require("harpoon")
+      -- REQUIRED
+      harpoon:setup()
+    end,
+
   },
   -- Programming
   {
