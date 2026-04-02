79b54b5d5450916d01059b177b12c4908f586b24
Ryder McMinn <rdr@rdrmc.com>
2026-04-01 23:08:39 -0700

    feat(ghostty): add ghostty config with bytemancer theme and keybinds

--- 02_stow.sh
+++ 02_stow.sh
@@ -4,6 +4,7 @@ stow alacritty -t ~/ --verbose=2
 stow claude -t ~/ --verbose=2
 stow emacs -t ~/ --verbose=2
 stow fish -t ~/ --verbose=2
+stow ghostty -t ~/ --verbose=2
 stow nvim -t ~/ --verbose=2
 stow tmux -t ~/ --verbose=2
 stow zed -t ~/ --verbose=2
