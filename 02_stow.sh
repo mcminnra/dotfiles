98a57a6bee90db4dd9e4388ad45cd9981f4cd572
Ryder McMinn <ryder.mcminn@sony.com>
2026-06-09 16:47:30 -0700

    chore: fix stow pathing for work

--- 02_stow.sh
+++ 02_stow.sh
@@ -9,8 +9,15 @@ stow nvim -t ~/ --verbose=2
 stow tmux -t ~/ --verbose=2
 stow zed -t ~/ --verbose=2
 
-# === Condionally ask for some folders
-# git
+# Personal vs Work Stows
+read -p "Work or Personal? (w/p): " answer
+if [[ "$answer" =~ ^[Ww]$ ]]; then
+    # Keep work claude stuff out of git
+else
+    stow claude-personal -t ~/ --verbose=2
+fi
+
+# git (by os)
 read -p "Do you want to stow git? (y/n): " answer
 if [[ "$answer" =~ ^[Yy]$ ]]; then
     stow git -t ~/ --verbose=2
